fb216c5300ada2c391d0267a27eaad4a8693b791
Vishnu S <68739148+S0Vishnu@users.noreply.github.com>
2025-01-19 02:14:27 +0530

    ui update

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./assets/styles.scss";
 import NavBar from "./components/navBar";
 import CatModel from "./components/catModel";
 
-// import { vector2 } from "./asstes/images/Vector_2.svg";
-// import { vector3 } from "./asstes/images/Vector_3.svg";
+import vector2 from "./assets/images/Vector_2.svg";
+import vector3 from "./assets/images/Vector_3.svg";
 import Button from "./components/button";
 import DogModel from "./components/dogModel";
 
@@ -22,13 +22,13 @@ function App() {
                 Taking <CatModel /> Care <br />
                 of Your Little <br />
                 Friends
-                {/* <img src={vector3} alt="vector3" /> */}
+                <img src={vector3} alt="vector3" className="img1"/>
               </div>
               <div className="sub-content">
                 Our highly skilled profetional associates love pets as much as
                 you do, and we offer a wide range of pet services.
               </div>
-              {/* <img src={vector2} alt="vector1" /> */}
+              <img src={vector2} alt="vector1" className="img2"/>
             </div>
             <Button
               value={"Book a Schedule"}
